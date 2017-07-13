19fb562b399bd907138bc02c1726c0bcff7af416
Shubham24492 <shubhusarawade@gmail.com>
2017-07-13 12:13:53 +0530

    second commit

--- src/mypackage/StudentAdmissionController.java
+++ src/mypackage/StudentAdmissionController.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import javax.security.auth.message.callback.PrivateKeyCallback.Request;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,22 +21,31 @@ public class StudentAdmissionController {
     public ModelAndView getAdmissionForm() {
 
         ModelAndView modelandview = new ModelAndView("AdmissionForm");
+        //modelandview.addObject("headerMsg", "Admission Form");
 
         return modelandview;
     }
+	
+	@ModelAttribute
+	public void addingCommonObjects(Model model1)
+	{
+		model1.addAttribute("headerMsg", "Admission Form");
+	}
+	
 	@RequestMapping(value = "/submitAdmissionForm.html", method = RequestMethod.POST)
     //public ModelAndView submitAdmissionForm(@RequestParam("studentName") String name, @RequestParam("studentHobby") String hobby) {
-	public ModelAndView submitAdmissionForm(@RequestParam Map<String, String> reqParams) {
-	 	String userName = reqParams.get("studentName");
-		String Hobby = reqParams.get("studentHobby");
+	//public ModelAndView submitAdmissionForm(@RequestParam Map<String, String> reqParams) {
+	public ModelAndView submitAdmissionForm(@ModelAttribute("studentObj") Student std) {
+	 	//String userName = reqParams.get("studentName");
+		//String Hobby = reqParams.get("studentHobby");
 		
-        Student std = new Student();
-        std.setStudentName(userName);
-        std.setStudentHobby(Hobby);
+        //Student std = new Student();
+        //std.setStudentName(userName);
+        //std.setStudentHobby(Hobby);
         
         ModelAndView modelandview = new ModelAndView("AdmissionSuccess");
-        modelandview.addObject("headerMsg", "Student Details");
-        modelandview.addObject("studentObj", std);
+        //modelandview.addObject("headerMsg", "Student Details");
+        //modelandview.addObject("studentObj", std);
         
 		//modelandview.addObject("message", "Hello "+ userName + ", your hobby: "+ Hobby);
 
